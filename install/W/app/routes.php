9c31a6702331621d6cb993bc58c64212df401f97
Darkers54 <alex.omel@outlook.com>
2016-01-15 01:07:16 +0100

    Ajout de pages statiques (preview) + integration bootstrap et font legothick

--- install/W/app/routes.php
+++ install/W/app/routes.php
@@ -2,4 +2,10 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
+		['GET', '/presentation', 'Default#presentation', 'default_presentation'],
+		['GET', '/contact', 'Default#contact', 'default_contact'],
+		['GET', '/mentions', 'Default#mentions', 'default_mentions'],
+		['GET', '/help', 'Default#help', 'default_help'],
+
+		['GET', '/imex', 'ImEx#home', 'imex_home'],
 	);
