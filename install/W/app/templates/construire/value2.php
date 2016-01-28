43513a84f4348d3f147d7c5e90e01bffe6c5d9ca
Darkers54 <alex.omel@outlook.com>
2016-01-28 01:10:57 +0100

    RAZ branche TEST pour copie de DEV_AO traité après merge

--- install/W/app/templates/construire/value2.php
+++
@@ -1,3 +0,0 @@
-<?php foreach($monImage as $img) : ?>
-<img id="<?= $img['ID_prod'] ?>" src="<?= $this->assetUrl('/img/products/'.$img['pdctName'].'/'.$img['prodUrl'])?>" alt="<?= $img['prodDesc']?>">
-<?php endforeach; ?> 
