3115175336b4df5c78ef3b606b1f1b4cb7ad3c89
dylan bourreli <bourreli.dylan@hotmail.com>
2016-01-28 10:09:13 +0100

    RAZ Branche DEV pour réintégration de la branche TEST

--- install/W/app/templates/default/contact.php
+++
@@ -1,8 +0,0 @@
-<?php $this->layout('layout', ['title' => 'Contact']) ?>
-<?php $this->start('main_content') ?>
-	<h2>Contact</h2>
-	<p>Cela ne se voit pas encore, mais ceci est un formulaire de contact comportant les informations
-	nécessaires à l'enregistrement dans la table tbusers.</p>
-
-
-<?php $this->stop('main_content') ?>
--- install/W/public/eLEGO/wp-content/plugins/customer-area/src/php/core-addons/shortcodes/templates/protected-content-shortcode-layout-list-before.template.php
+++
@@ -1,3 +0,0 @@
-<?php /** Template version: 1.0.0 */ ?>
-
-<ul class="cuar-item-list">
